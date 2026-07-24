f0e79a29f05908bb8ba0b3949dcefd9b8351e1a8
weepwood <weepwood@outlook.com>
2026-07-24 23:20:06 +0800

    修复：接入安全分享提示

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { App } from './js/app.js'
 import { BucketManagerUI } from './js/bucket-manager.js'
+import { ShareConfigSecurityNotice } from './js/share-config-security.js'
 
 // Boot
 new App()
 new BucketManagerUI().init()
+new ShareConfigSecurityNotice().init()
