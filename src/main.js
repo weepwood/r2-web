efd9fb3bf6efbaf77b2f1bb446ed5f87299eeb49
weepwood <weepwood@outlook.com>
2026-07-25 11:59:35 +0800

    重构：在应用启动时安装平滑 Bucket 切换

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { App } from './js/app.js'
 import { BucketManagerUI } from './js/bucket-manager.js'
 import { ShareConfigSecurityNotice } from './js/share-config-security.js'
-import { initGlobalErrorHandler } from './js/global-error-handler.js'
+import { installSmoothBucketSwitch } from './js/smooth-bucket-switch.js'
+
+installSmoothBucketSwitch()
 
 // Boot
-initGlobalErrorHandler()
 new App()
 new BucketManagerUI().init()
 new ShareConfigSecurityNotice().init()
